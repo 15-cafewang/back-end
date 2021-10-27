c07a1dad0ed78e78c7b141ccaa23088e89ea8a7a
heejun <heejjuunn@gmail.com>
2021-10-28 00:05:33 +0900

    fix : RecipeRepository실수 수정

--- src/main/java/com/sparta/backend/repository/RecipesRepository.java
+++ src/main/java/com/sparta/backend/repository/RecipesRepository.java
@@ -1,4 +1,7 @@
 package com.sparta.backend.repository;
 
-public class RecipesRepository {
+import com.sparta.backend.domain.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecipesRepository extends JpaRepository<Recipe,Long> {
 }
