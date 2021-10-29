7b1e9e561e93579ed285c6421f8ea634b6bdf074
jhhong0930 <jhhong0930@gmail.com>
2021-10-29 14:21:48 +0900

    feat: UserDetailsServiceImpl 생성

---
+++ src/main/java/com/sparta/backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.sparta.backend.security;
+
+
+import com.sparta.backend.domain.User;
+import com.sparta.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Can't find " + email));
+
+        return new UserDetailsImpl(user);
+    }
+}
