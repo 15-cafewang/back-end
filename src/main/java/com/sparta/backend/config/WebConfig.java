9935b25268b6805e73e956d6d4a39be314612480
jhhong0930 <jhhong0930@gmail.com>
2021-10-29 14:24:00 +0900

    update: WebConfig 수정(CORS 설정)

--- src/main/java/com/sparta/backend/config/WebConfig.java
+++ src/main/java/com/sparta/backend/config/WebConfig.java
@@ -1,20 +1,18 @@
 package com.sparta.backend.config;
 
-import com.sparta.backend.domain.constant.EnumMapper;
-import com.sparta.backend.domain.constant.UserRole;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Order(1)
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public EnumMapper enumMapper() {
-        EnumMapper enumMapper = new EnumMapper();
-        enumMapper.put("UserRole", UserRole.class);
-
-        return enumMapper;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("POST", "GET", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowCredentials(true);
     }
+
 }
