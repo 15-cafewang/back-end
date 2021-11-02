f947272953b62c7ec508d9e91233b0492bbfdcf4
baenayoung <nybae2003@naver.com>
2021-11-02 19:22:08 +0900

    feat: 게시판 테이블 생성

---
+++ src/main/java/com/sparta/backend/domain/Board.java
@@ -0,0 +1,44 @@
+package com.sparta.backend.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@ToString(exclude = {"user"})
+@Getter
+@NoArgsConstructor
+@Entity
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recipe_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    private String image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonManagedReference
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    @JsonBackReference
+    private List<BoardComment> boardCommentList;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    @JsonBackReference
+    private List<BoardLikes> boardLikesList;
+
+}
