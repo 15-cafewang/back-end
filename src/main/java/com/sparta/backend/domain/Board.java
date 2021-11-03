6d855206f175124f9a1dade4037ca7d9c0dcff1e
baenayoung <nybae2003@naver.com>
2021-11-03 14:09:43 +0900

    update: 컬럼 이름 변경

--- src/main/java/com/sparta/backend/domain/Board.java
+++ src/main/java/com/sparta/backend/domain/Board.java
@@ -18,7 +18,7 @@ public class Board extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "recipe_id")
+    @Column(name = "board_id")
     private Long id;
 
     @Column(nullable = false)
