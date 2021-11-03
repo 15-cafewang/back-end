ec2b04528c17bcd715142ff210dccc23ad010e89
jhhong0930 <jhhong0930@gmail.com>
2021-11-03 11:04:31 +0900

    feat: nicnname에 unique 속성 추가

--- src/main/java/com/sparta/backend/domain/User.java
+++ src/main/java/com/sparta/backend/domain/User.java
@@ -23,7 +23,7 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String nickname;
 
     @Column(nullable = false)
