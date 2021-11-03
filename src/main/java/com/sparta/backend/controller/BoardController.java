861459e7a3bdc4f27122856f3e09d7e04561d5fe
baenayoung <nybae2003@naver.com>
2021-11-03 14:21:29 +0900

    feat: 게시물 삭제 기능 추가

--- src/main/java/com/sparta/backend/controller/BoardController.java
+++ src/main/java/com/sparta/backend/controller/BoardController.java
@@ -6,6 +6,8 @@ import com.sparta.backend.security.UserDetailsImpl;
 import com.sparta.backend.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,10 +27,22 @@ public class BoardController {
         Long boardId = boardService.createBoard(requestDto, userDetails);
 
         if(boardId > 0) {
-            return new CustomResponseDto<>(1, "게시물 등록 완료", "");
+            return new CustomResponseDto<>(1, "게시물 등록 성공", "");
         } else {
             return new CustomResponseDto<>(-1, "게시물 등록 실패", "");
         }
     }
 
+    //게시물 삭제
+    @DeleteMapping("/boards/{boardId}")
+    public CustomResponseDto<?> deleteBoard(@PathVariable("boardId") Long id,
+                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long boardId = boardService.deleteBoard(id, userDetails);
+
+        if(boardId > 0) {
+            return new CustomResponseDto<>(1, "게시물 삭제 성공", "");
+        } else {
+            return new CustomResponseDto<>(-1, "게시물 삭제 실패", "");
+        }
+    }
 }
--- src/main/java/com/sparta/backend/service/BoardService.java
+++ src/main/java/com/sparta/backend/service/BoardService.java
@@ -1,5 +1,6 @@
 package com.sparta.backend.service;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.sparta.backend.awsS3.S3Uploader;
 import com.sparta.backend.domain.Board;
@@ -9,6 +10,7 @@ import com.sparta.backend.repository.BoardRepository;
 import com.sparta.backend.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
@@ -43,4 +45,38 @@ public class BoardService {
 
         return boardId;
     }
+
+    //게시물 삭제
+    @Transactional
+    public Long deleteBoard(Long id, UserDetailsImpl userDetails) {
+        String currentLoginEmail = userDetails.getUser().getEmail();
+        Board board = boardRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("찾는 게시물이 없습니다.")
+        );
+        String writerEmail = board.getUser().getEmail();
+
+        if(writerEmail.equals(currentLoginEmail)) {
+            deleteS3(board.getImage());
+            boardRepository.deleteById(id);
+        } else {
+            throw new IllegalArgumentException("게시물을 작성한 사용자만 삭제 가능합니다.");
+        }
+
+        return board.getId();
+    }
+
+    //S3 이미지 삭제
+    public void deleteS3(String imageName){
+        //https://S3 버킷 URL/버킷에 생성한 폴더명/이미지이름
+        String keyName = imageName.split("/")[4]; // 이미지이름만 추출
+
+        try {
+            amazonS3Client.deleteObject(bucket + "/boardImage", keyName);
+            System.out.println("S3에서 삭제한 파일 이름: " + keyName);
+        }catch (AmazonServiceException e){
+            e.printStackTrace();
+            throw new AmazonServiceException(e.getMessage());
+        }
+    }
+
 }
