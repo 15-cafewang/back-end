ac4e4db6f1694c0bcf669668b784ec2a8acb51a9
jhhong0930 <jhhong0930@gmail.com>
2021-11-03 15:49:58 +0900

    feat: UserInfoResponseDto 생성

---
+++ src/main/java/com/sparta/backend/dto/response/user/UserInfoResponseDto.java
@@ -0,0 +1,12 @@
+package com.sparta.backend.dto.response.user;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoResponseDto {
+
+    private String nickname;
+    private String image;
+}
