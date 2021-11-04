2c84337e9c89cdd351a0bf2e8d022ceeb9f96966
jhhong0930 <jhhong0930@gmail.com>
2021-11-04 15:42:52 +0900

    feat: getter 추가

--- src/main/java/com/sparta/backend/dto/response/user/UserInfoResponseDto.java
+++ src/main/java/com/sparta/backend/dto/response/user/UserInfoResponseDto.java
@@ -1,8 +1,10 @@
 package com.sparta.backend.dto.response.user;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserInfoResponseDto {
