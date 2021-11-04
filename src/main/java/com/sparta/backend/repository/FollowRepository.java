36947ad3ea98f77a37e382730f0c40c20fcc06ff
jhhong0930 <jhhong0930@gmail.com>
2021-11-04 17:33:58 +0900

    feat: findByFromUserAndToUser 메소드 추가

--- src/main/java/com/sparta/backend/repository/FollowRepository.java
+++ src/main/java/com/sparta/backend/repository/FollowRepository.java
@@ -1,7 +1,12 @@
 package com.sparta.backend.repository;
 
 import com.sparta.backend.domain.Follow;
+import com.sparta.backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+    Optional<Follow> findByFromUserAndToUser(User fromUser, User toUser);
 }
