2888d4c027c3b465ffdbea827b1fb5a76253306e
jhhong0930 <jhhong0930@gmail.com>
2021-11-07 02:42:41 +0900

    feat: GetRecipeListResponseDto 생성

---
+++ src/main/java/com/sparta/backend/dto/response/userinfo/GetRecipeListResponseDto.java
@@ -0,0 +1,33 @@
+package com.sparta.backend.dto.response.userinfo;
+
+import com.sparta.backend.domain.Recipe.Recipe;
+import com.sparta.backend.security.UserDetailsImpl;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetRecipeListResponseDto {
+
+    private Long recipeId;
+    private String title;
+    private String nickname;
+    private int price;
+    private List<String> imageList = new ArrayList<>();
+    private int likeCount;
+
+    public GetRecipeListResponseDto(Recipe recipe, UserDetailsImpl userDetails) {
+        this.recipeId = recipe.getId();
+        this.title = recipe.getTitle();
+        this.nickname = recipe.getUser().getNickname();
+        this.price = recipe.getPrice();
+        recipe.getRecipeImagesList().forEach((RecipeImage) -> this.imageList.add(RecipeImage.getImage()));
+        this.likeCount = recipe.getRecipeLikesList().size();
+    }
+
+}
