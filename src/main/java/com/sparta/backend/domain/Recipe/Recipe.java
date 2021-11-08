0be7490dfeeafe0658c26f0336417df5678afc31
heejun <heejjuunn@gmail.com>
2021-11-08 16:08:13 +0900

    fix: 클래스명 오타 수정

--- src/main/java/com/sparta/backend/domain/Recipe/Recipe.java
+++ src/main/java/com/sparta/backend/domain/Recipe/Recipe.java
@@ -5,14 +5,12 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.sparta.backend.domain.BaseEntity;
 import com.sparta.backend.domain.Tag;
 import com.sparta.backend.domain.User;
-import com.sparta.backend.security.UserDetailsImpl;
-import com.sparta.backend.validator.ProductValidator;
+import com.sparta.backend.validator.RecipeValidator;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @ToString(exclude = {"user"})
@@ -56,7 +54,7 @@ public class Recipe extends BaseEntity {
 
     public Recipe(String title, String content, int price, User user){
         //Edge케이스들 validation
-        ProductValidator.validateRecipeInput(title,content,user);
+        RecipeValidator.validateRecipeInput(title,content,user);
         this.title = title;
         this.content = content;
         this.price = price;
--- src/main/java/com/sparta/backend/validator/RecipeValidator.java
+++ src/main/java/com/sparta/backend/validator/RecipeValidator.java
@@ -2,7 +2,7 @@ package com.sparta.backend.validator;
 
 import com.sparta.backend.domain.User;
 
-public class ProductValidator {
+public class RecipeValidator {
     public static void validateRecipeInput(String title, String content, User user){
         if(user == null){
             throw  new IllegalArgumentException("로그인 되지 않은 사용자입니다");
@@ -15,7 +15,6 @@ public class ProductValidator {
             throw new IllegalArgumentException("제목이 입력되지 않았습니다.");
         }
 
-        //todo title이나 글자길이도 신경써야 함.
         if(title.length() > 200){
             throw new IllegalArgumentException("제목의 길이가 200자를 초과하였습니다.");
         }
