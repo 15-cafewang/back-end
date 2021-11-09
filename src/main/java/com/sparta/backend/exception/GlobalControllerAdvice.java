55b20a69f8d773cb3357a02f042ad28074341b90
heejun <heejjuunn@gmail.com>
2021-11-09 17:47:55 +0900

    update: 예외처리 응답형식 통일

--- src/main/java/com/sparta/backend/exception/GlobalControllerAdvice.java
+++ src/main/java/com/sparta/backend/exception/GlobalControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.sparta.backend.exception;
 
+import com.sparta.backend.dto.response.CustomResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,22 +13,29 @@ public class GlobalControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NullPointerException.class)
-    public ErrorResult nullExHandle(NullPointerException e) {
+    public CustomResponseDto<?> nullExHandle(NullPointerException e) {
 
-        return new ErrorResult("BAD", e.getMessage());
+        return new CustomResponseDto<>(-1, e.getMessage(),"");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandle(IllegalArgumentException e) {
+    public CustomResponseDto<?> illegalExHandle(IllegalArgumentException e) {
 
-        return new ErrorResult("BAD", e.getMessage());
+        return new CustomResponseDto<>(-1, e.getMessage(),"");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NoSuchElementException.class)
-    public ErrorResult noSuchElemHandle(NoSuchElementException e) {
+    public CustomResponseDto<?> noSuchElemHandle(NoSuchElementException e) {
 
-        return new ErrorResult("BAD", e.getMessage());
+        return new CustomResponseDto<>(-1, e.getMessage(),"");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(CustomErrorException.class)
+    public CustomResponseDto<?> noSuchElemHandle(CustomErrorException e) {
+
+        return new CustomResponseDto<>(-1, e.getMessage(),"");
     }
 }
