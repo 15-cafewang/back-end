e9ae75fa1bd654aa34385deaa3905000c6d16c90
jhhong0930 <jhhong0930@gmail.com>
2021-11-10 16:00:57 +0900

    test: User 엔티티 테스트 추가

--- src/test/java/com/sparta/backend/domain/user/UserTest.java
+++ src/test/java/com/sparta/backend/domain/user/UserTest.java
@@ -2,7 +2,6 @@ package com.sparta.backend.domain.user;
 
 import com.sparta.backend.domain.User;
 import com.sparta.backend.domain.UserRole;
-import com.sparta.backend.validator.UserValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,296 +10,97 @@ import static org.assertj.core.api.Assertions.*;
 
 class UserTest {
 
-    @Test
+    @Nested
     @DisplayName("회원 객체 생성")
-    void testUser() {
-
-        // given
-        String email = "aaa@aaa.com";
-        String password = "1234";
-        String nickname = "test";
-        String image = "image";
-        UserRole role = UserRole.USER;
-        String status = "Y";
-
-        // when
-        User user = new User(email, password, nickname, image, role, status);
-
-        // then
-        assertThat(email).isEqualTo(user.getEmail());
-        assertThat(password).isEqualTo(user.getPassword());
-        assertThat(nickname).isEqualTo(user.getNickname());
-        assertThat(image).isEqualTo(user.getImage());
-        assertThat(role).isEqualTo(user.getRole());
-        assertThat(status).isEqualTo(user.getStatus());
-    }
-
-    @Nested
-    @DisplayName("이메일 검증 테스트")
-    class EmailTest {
-
-        @Test
-        @DisplayName("성공 케이스")
-        void emailSuccess() {
-
-            // given
-            String email = "aaa@naver.com";
-
-            // when then
-            assertThat(UserValidator.validateEmail(email)).isTrue();
-        }
-
-        @Nested
-        @DisplayName("실패 케이스")
-        class EmailFail {
-
-            @Test
-            @DisplayName("입력값이 null일 경우")
-            void emailFail_null() {
-
-                // given
-                String email = "";
-
-                // when then
-                assertThatThrownBy(() -> {
-                    UserValidator.validateEmail(email);
-                }).isInstanceOf(IllegalArgumentException.class);
-            }
-
-            @Test
-            @DisplayName("@가 포함되지 않을경우")
-            void emailFail_notIncludeAt() {
-
-                // given
-                String email = "asd123";
-
-                // when then
-                assertThatThrownBy(() -> {
-                    UserValidator.validateEmail(email);
-                }).isInstanceOf(IllegalArgumentException.class);
-            }
-
-            @Test
-            @DisplayName(".이 포함되지 않을경우")
-            void emailFail_notIncludeDot() {
-
-                // given
-                String email = "asd123@asd";
-
-                // when then
-                assertThatThrownBy(() -> {
-                    UserValidator.validateEmail(email);
-                }).isInstanceOf(IllegalArgumentException.class);
-            }
-
-            @Test
-            @DisplayName(".뒤에 두글자 이상이 아닌경우")
-            void emailFail_notEnoughAfterDot() {
-
-                // given
-                String email = "asd123@asd.c";
-
-                // when then
-                assertThatThrownBy(() -> {
-                    UserValidator.validateEmail(email);
-                }).isInstanceOf(IllegalArgumentException.class);
-            }
-        }
-    }
-
-    @Nested
-    @DisplayName("비밀번호 검증 테스트")
-    class PasswordTest {
-
-        @Test
-        @DisplayName("성공 테스트")
-        void passwordSuccess() {
-
-            // given
-            String password = "qweQWE123!@#";
-
-            // when then
-            assertThat(UserValidator.validatePassword(password)).isTrue();
-        }
-
-        @Nested
-        @DisplayName("실패 케이스")
-        class PasswordFail {
-
-            @Test
-            @DisplayName("길이가 8보다 작은 경우")
-            void passwordFail_lessThan8() {
-
-                // given
-                String password = "qweQW1!";
-
-                // when then
-                assertThatThrownBy(() -> {
-                    UserValidator.validatePassword(password);
-                }).isInstanceOf(IllegalArgumentException.class);
-            }
-
-            @Test
-            @DisplayName("길이가 20보다 큰 경우")
-            void passwordFail_moreThan20() {
-
-                // given
-                String password = "qqwweeQQWWEE112233!!@";
-
-                // when then
-                assertThatThrownBy(() -> {
-                    UserValidator.validatePassword(password);
-                }).isInstanceOf(IllegalArgumentException.class);
-            }
-
-            @Test
-            @DisplayName("숫자가 포함되지 않은 경우")
-            void passwordFail_withoutDigit() {
-
-                // given
-                String password = "qweQWE!@#";
-
-                // when then
-                assertThatThrownBy(() -> {
-                    UserValidator.validatePassword(password);
-                }).isInstanceOf(IllegalArgumentException.class);
-            }
-
-            @Test
-            @DisplayName("영문 소문자가 포함되지 않은 경우")
-            void passwordFail_withoutLowerCase() {
-
-                // given
-                String password = "QWE123!@#";
-
-                // when then
-                assertThatThrownBy(() -> {
-                    UserValidator.validatePassword(password);
-                }).isInstanceOf(IllegalArgumentException.class);
-            }
-
-            @Test
-            @DisplayName("영문 대문자가 포함되지 않은 경우")
-            void passwordFail_withoutUpperCase() {
-
-                // given
-                String password = "qwe123!@#";
-
-                // when then
-                assertThatThrownBy(() -> {
-                    UserValidator.validatePassword(password);
-                }).isInstanceOf(IllegalArgumentException.class);
-            }
-
-            @Test
-            @DisplayName("특수문자가 포함되지 않은 경우")
-            void passwordFail_withoutSpecialSymbol  () {
-
-                // given
-                String password = "qweQWE123";
-
-                // when then
-                assertThatThrownBy(() -> {
-                    UserValidator.validatePassword(password);
-                }).isInstanceOf(IllegalArgumentException.class);
-            }
-        }
-    }
-
-    @Nested
-    @DisplayName("닉네임 검증 테스트")
-    class NicknameTest {
+    class CreateUser {
 
         @Nested
         @DisplayName("성공 케이스")
-        class NicknameSuccess {
+        class UserSuccess {
 
             @Test
-            @DisplayName("숫자만 사용하는 경우")
-            void nicknameSuccess_onlyDigit() {
+            void userTest() {
 
                 // given
-                String nickname = "12345678";
+                String email = "aaa@aaa.com";
+                String password = "qweQWE123!@#";
+                String nickname = "test";
+                String image = "image";
+                UserRole role = UserRole.USER;
+                String status = "Y";
 
-                // when then
-                assertThat(UserValidator.validateNickname(nickname)).isTrue();
-            }
+                // when
+                User user = new User(email, password, nickname, image, role, status);
 
-            @Test
-            @DisplayName("영문만 사용하는 경우")
-            void nicknameSuccess_onlyEnglish() {
-
-                // given
-                String nickname = "nickName";
-
-                // when then
-                assertThat(UserValidator.validateNickname(nickname)).isTrue();
-            }
-
-            @Test
-            @DisplayName("한글만 사용하는 경우")
-            void nicknameSuccess_onlyKorean() {
-
-                // given
-                String nickname = "한글도가능";
-
-                // when then
-                assertThat(UserValidator.validateNickname(nickname)).isTrue();
-            }
-
-            @Test
-            @DisplayName("복합으로 사용하는 경우")
-            void nicknameSuccess_mixed() {
-
-                // given
-                String nickname = "qW무야호123";
-
-                // when then
-                assertThat(UserValidator.validateNickname(nickname)).isTrue();
+                // then
+                assertThat(email).isEqualTo(user.getEmail());
+                assertThat(password).isEqualTo(user.getPassword());
+                assertThat(nickname).isEqualTo(user.getNickname());
+                assertThat(image).isEqualTo(user.getImage());
+                assertThat(role).isEqualTo(user.getRole());
+                assertThat(status).isEqualTo(user.getStatus());
             }
         }
 
         @Nested
         @DisplayName("실패 케이스")
-        class NicknameFail {
+        class UserFail {
 
             @Test
-            @DisplayName("길이가 2보다 작은 경우")
-            void nicknameFail_lessThan2() {
+            @DisplayName("이메일 형식이 잘못된 경우")
+            void emailError() {
 
                 // given
-                String nickname = "1";
+                String email = "aaa";
+                String password = "qweQWE123!@#";
+                String nickname = "test";
+                String image = "image";
+                UserRole role = UserRole.USER;
+                String status = "Y";
 
                 // when then
                 assertThatThrownBy(() -> {
-                    UserValidator.validateNickname(nickname);
-                }).isInstanceOf(IllegalArgumentException.class);
+                    new User(email, password, nickname, image, role, status);
+                }).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("이메일 형식이 아닙니다");
             }
 
             @Test
-            @DisplayName("길이가 8보다 큰 경우")
-            void nicknameFail_moreThan8() {
+            @DisplayName("비밀번호 형식이 잘못된 경우")
+            void passwordError() {
 
                 // given
-                String nickname = "asdqwe121";
+                String email = "aaa@aaa.com";
+                String password = "qwe123";
+                String nickname = "test";
+                String image = "image";
+                UserRole role = UserRole.USER;
+                String status = "Y";
 
                 // when then
                 assertThatThrownBy(() -> {
-                    UserValidator.validateNickname(nickname);
-                }).isInstanceOf(IllegalArgumentException.class);
+                    new User(email, password, nickname, image, role, status);
+                }).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("비밀번호는 숫자, 영문 소문자, 대문자, 특수문자를 하나씩 포함한 8자이상 20자 이하여야합니다");
             }
 
             @Test
-            @DisplayName("특수문자를 포함한 경우")
-            void nicknameFail_withSpecialSymbol() {
+            @DisplayName("닉네임 형식이 잘못된 경우")
+            void nicknameError() {
 
                 // given
-                String nickname = "qwe123@";
+                String email = "aaa@aaa.com";
+                String password = "qweQWE123!@#";
+                String nickname = "tester@@";
+                String image = "image";
+                UserRole role = UserRole.USER;
+                String status = "Y";
 
                 // when then
                 assertThatThrownBy(() -> {
-                    UserValidator.validateNickname(nickname);
-                }).isInstanceOf(IllegalArgumentException.class);
+                    new User(email, password, nickname, image, role, status);
+                }).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("닉네임은 특수문자를 포함하지 않은 2자 이상 8자 이하여야합니다");
             }
         }
     }
