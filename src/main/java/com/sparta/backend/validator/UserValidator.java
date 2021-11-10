b25f25ab6ecf487e873316bb8a0905d51eccc676
jhhong0930 <jhhong0930@gmail.com>
2021-11-10 14:22:44 +0900

    feat: 닉네임 검증 로직 추가

--- src/main/java/com/sparta/backend/validator/UserValidator.java
+++ src/main/java/com/sparta/backend/validator/UserValidator.java
@@ -21,5 +21,14 @@ public class UserValidator {
 
         return true;
     }
+
+    public static boolean validateNickname(String nickname) {
+
+        if (!Pattern.matches("^([0-9a-zA-Z가-힣]{2,8})$", nickname)) {
+            throw new IllegalArgumentException("닉네임은 특수문자를 포함하지 않은 2자 이상 8자 이하여야합니다");
+        }
+
+        return true;
+    }
     
 }
