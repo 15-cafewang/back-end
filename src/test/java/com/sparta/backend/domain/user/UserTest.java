297568f5979cfc67ca83a2dbce75e95c46bf16da
jhhong0930 <jhhong0930@gmail.com>
2021-11-10 12:59:39 +0900

    test: 이메일 검증 테스트 추가

--- src/test/java/com/sparta/backend/domain/user/UserTest.java
+++ src/test/java/com/sparta/backend/domain/user/UserTest.java
@@ -2,7 +2,9 @@ package com.sparta.backend.domain.user;
 
 import com.sparta.backend.domain.User;
 import com.sparta.backend.domain.UserRole;
+import com.sparta.backend.validator.UserValidator;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -31,6 +33,79 @@ class UserTest {
         assertThat(image).isEqualTo(user.getImage());
         assertThat(role).isEqualTo(user.getRole());
         assertThat(status).isEqualTo(user.getStatus());
+    }
+
+    @Nested
+    @DisplayName("이메일 검증 테스트")
+    class EmailTest {
+
+        @Test
+        @DisplayName("성공 케이스")
+        void emailSuccess() {
+
+            // given
+            String email = "aaa@naver.com";
+
+            // when then
+            assertThat(UserValidator.validateEmail(email)).isTrue();
+        }
+
+        @Nested
+        @DisplayName("실패 케이스")
+        class EmailFail {
+
+            @Test
+            @DisplayName("입력값이 null일 경우")
+            void emailFail_null() {
+
+                // given
+                String email = "";
+
+                // when then
+                assertThatThrownBy(() -> {
+                    UserValidator.validateEmail(email);
+                }).isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @Test
+            @DisplayName("@가 포함되지 않을경우")
+            void emailFail_notIncludeAt() {
+
+                // given
+                String email = "asd123";
+
+                // when then
+                assertThatThrownBy(() -> {
+                    UserValidator.validateEmail(email);
+                }).isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @Test
+            @DisplayName(".이 포함되지 않을경우")
+            void emailFail_notIncludeDot() {
+
+                // given
+                String email = "asd123@asd";
+
+                // when then
+                assertThatThrownBy(() -> {
+                    UserValidator.validateEmail(email);
+                }).isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @Test
+            @DisplayName(".뒤에 두글자 이상이 아닌경우")
+            void emailFail_notEnoughAfterDot() {
+
+                // given
+                String email = "asd123@asd.c";
+
+                // when then
+                assertThatThrownBy(() -> {
+                    UserValidator.validateEmail(email);
+                }).isInstanceOf(IllegalArgumentException.class);
+            }
+        }
     }
 
 }
