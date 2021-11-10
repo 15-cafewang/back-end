df4713223c3d214894fd9cb5f26fc80dc3fbb99d
jhhong0930 <jhhong0930@gmail.com>
2021-11-10 12:33:14 +0900

    feat: 이메일 검증 로직 추가

--- src/main/java/com/sparta/backend/validator/UserValidator.java
+++ src/main/java/com/sparta/backend/validator/UserValidator.java
@@ -1,4 +1,16 @@
 package com.sparta.backend.validator;
 
+import java.util.regex.Pattern;
+
 public class UserValidator {
+
+    public static boolean validateEmail(String email) {
+
+        if (!Pattern.matches("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", email)) {
+            throw new IllegalArgumentException("이메일 형식이 아닙니다");
+        }
+
+        return true;
+    }
+    
 }
