a93af878f26defb3a7181ec227765a4c42568185
heejun <heejjuunn@gmail.com>
2021-11-11 10:09:11 +0900

    test: controllerTest- 레시피 등록 dto 제목 테스트

--- src/main/java/com/sparta/backend/validator/PostRecipeRequestDtoValidator.java
+++ src/main/java/com/sparta/backend/validator/PostRecipeRequestDtoValidator.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public class PostRecipeRequestDtoValidator {
     public static void validateRecipeInput(PostRecipeRequestDto requestDto){
-        if(requestDto.getTitle() == null){
+        if(requestDto.getTitle() == null || requestDto.getTitle().trim().equals("")){
             throw new IllegalArgumentException("제목은 필수입니다.");
+        }
+
+        if(requestDto.getTitle().length() > 100){
+            throw new IllegalArgumentException("제목이 너무 깁니다.");
         }
     }
 
--- src/test/java/com/sparta/backend/mvc/RecipeControllerTest.java
+++ src/test/java/com/sparta/backend/mvc/RecipeControllerTest.java
@@ -11,6 +11,7 @@ import com.sparta.backend.service.recipe.TagService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -82,7 +83,7 @@ public class RecipeControllerTest {
                 .build();
     }
 
-    private void mockUserSetup() {
+    public void mockUserSetup() {
 // Mock 테스트 유져 생성
         String nickname = "testUser";
         String password = "hope!@#";
@@ -94,24 +95,113 @@ public class RecipeControllerTest {
         mockPrincipal = new UsernamePasswordAuthenticationToken(testUserDetails, "", testUserDetails.getAuthorities());
     }
 
-    //todo:
-    @Test
-    @DisplayName("레시피 등록")
-    void saveRecipe() throws Exception {
-        this.mockUserSetup();
-        MockMultipartFile image = new MockMultipartFile("image", "imagefile.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
-        MockMultipartFile image2 = new MockMultipartFile("image", "imagefile2.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/recipes")
-                        .file(image)
-                        .file(image2)
-//                        .param("title", "this is title")
-                        .param("content", "this is content")
-                        .principal(mockPrincipal))
-//                .andExpect(status().isOk());
-                .andExpect(status().is4xxClientError())
-                .andExpect(result -> assertEquals("제목은 필수입니다.",result.getResolvedException().getMessage()));
+    @Nested
+    @DisplayName("정상 케이스")
+    class SuccessCases {
+        @Test
+        @DisplayName("레시피 등록")
+        void saveRecipe() throws Exception {
+            mockUserSetup();
+            MockMultipartFile image = new MockMultipartFile("image", "imagefile.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
+            MockMultipartFile image2 = new MockMultipartFile("image", "imagefile2.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
+            mockMvc.perform(MockMvcRequestBuilders.multipart("/recipes")
+                            .file(image)
+                            .file(image2)
+                            .param("title", "this is title")
+                            .param("content", "this is content")
+                            .principal(mockPrincipal))
+                    .andExpect(status().isOk());
+
+        }
+    }
+
+    @Nested
+    @DisplayName("실패 케이스")
+    class FailCases {
+
+        @Nested
+        @DisplayName("title입력관련")
+        class titleRelated{
+            @Test
+            @DisplayName("title null")
+            void titleNUll() throws Exception {
+                mockUserSetup();
+                MockMultipartFile image = new MockMultipartFile("image", "imagefile.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
+                MockMultipartFile image2 = new MockMultipartFile("image", "imagefile2.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
+                mockMvc.perform(MockMvcRequestBuilders.multipart("/recipes")
+                                .file(image)
+                                .file(image2)
+                                .param("content", "this is content")
+                                .principal(mockPrincipal))
+                        .andExpect(status().is4xxClientError())
+                        .andExpect(result -> assertEquals("제목은 필수입니다.", result.getResolvedException().getMessage()));
+            }
+
+            @Test
+            @DisplayName("title empty")
+            void titleEmpty() throws Exception {
+                mockUserSetup();
+                MockMultipartFile image = new MockMultipartFile("image", "imagefile.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
+                MockMultipartFile image2 = new MockMultipartFile("image", "imagefile2.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
+                mockMvc.perform(MockMvcRequestBuilders.multipart("/recipes")
+                                .file(image)
+                                .file(image2)
+                                .param("title","")
+                                .param("content", "this is content")
+                                .principal(mockPrincipal))
+                        .andExpect(status().is4xxClientError())
+                        .andExpect(result -> assertEquals("제목은 필수입니다.", result.getResolvedException().getMessage()));
+            }
+
+            @Test
+            @DisplayName("titleTrim empty")
+            void titleTrimEmpty() throws Exception {
+                mockUserSetup();
+                MockMultipartFile image = new MockMultipartFile("image", "imagefile.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
+                MockMultipartFile image2 = new MockMultipartFile("image", "imagefile2.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
+                mockMvc.perform(MockMvcRequestBuilders.multipart("/recipes")
+                                .file(image)
+                                .file(image2)
+                                .param("title","    ")
+                                .param("content", "this is content")
+                                .principal(mockPrincipal))
+                        .andExpect(status().is4xxClientError())
+                        .andExpect(result -> assertEquals("제목은 필수입니다.", result.getResolvedException().getMessage()));
+            }
+
+            @Test
+            @DisplayName("title too long")
+            void titleTooLong() throws Exception {
+                mockUserSetup();
+                MockMultipartFile image = new MockMultipartFile("image", "imagefile.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
+                MockMultipartFile image2 = new MockMultipartFile("image", "imagefile2.jpeg", "image/jpg", new FileInputStream("src/test/java/com/sparta/backend/images/puppy1.jpg"));
+                mockMvc.perform(MockMvcRequestBuilders.multipart("/recipes")
+                                .file(image)
+                                .file(image2)
+                                .param("title", "If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play. By inputting the desired number, you can make a list of as many random sentences as you want or need. Producing random sentences can be helpful in a number of different ways.\n" +
+                                        "\n" +
+                                        "For writers, a random sentence can help them get their creative juices flowing. Since the topic of the sentence is completely unknown, it forces the writer to be creative when the sentence appears. There are a number of different ways a writer can use the random sentence for creativity. The most common way to use the sentence is to begin a story. Another option is to include it somewhere in the story. A much more difficult challenge is to use it to end a story. In any of these cases, it forces the writer to think creatively since they have no idea what sentence will appear from the tool.\n" +
+                                        "\n" +
+                                        "For those writers who have writers' block, this can be an excellent way to take a step to crumbling those walls. By taking the writer away from the subject matter that is causing the block, a random sentence may allow them to see the project they're working on in a different light and perspective. Sometimes all it takes is to get that first sentence down to help break the block.\n" +
+                                        "\n" +
+                                        "It can also be successfully used as a daily exercise to get writers to begin writing. Being shown a random sentence and using it to complete a paragraph each day can be an excellent way to begin any writing session.\n" +
+                                        "\n" +
+                                        "Random sentences can also spur creativity in other types of projects being done. If you are trying to come up with a new concept, a new idea or a new product, a random sentence may help you find unique qualities you may not have considered. Trying to incorporate the sentence into your project can help you look at it in different and unexpected ways than you would normally on your own.\n" +
+                                        "\n" +
+                                        "It can also be a fun way to surprise others. You might choose to share a random sentence on social media just to see what type of reaction it garners from others. It's an unexpected move that might create more conversation than a typical post or tweet.\n" +
+                                        "\n")
+                                .param("content", "this is content")
+                                .principal(mockPrincipal))
+                        .andExpect(status().is4xxClientError())
+                        .andExpect(result -> assertEquals("제목이 너무 깁니다.", result.getResolvedException().getMessage()));
+            }
+
+
+        }
+
 
     }
+
 
 //    @Test
 //    @DisplayName("레시피등록-validator")
@@ -127,7 +217,7 @@ public class RecipeControllerTest {
 //        //when
 //        Set<ConstraintViolation<PostRecipeRequestDto>> validate = validatorInjected.validate(requestDto);
 //
-        // then
+    // then
 //        Iterator<ConstraintViolation<PostRecipeRequestDto>> iterator = validate.iterator();
 //        List<String> messages = new ArrayList<>();
 //        while (iterator.hasNext()) {
