e189d4033a444ce0bf8b3befc4634cf7e4a307a4
jhhong0930 <jhhong0930@gmail.com>
2021-11-15 12:57:05 +0900

    feat: s3 cors 추가

--- src/main/java/com/sparta/backend/config/WebConfig.java
+++ src/main/java/com/sparta/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://mycipe.shop.s3-website.ap-northeast-2.amazonaws.com")
                 .allowedMethods("POST", "GET", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowCredentials(true);
     }
