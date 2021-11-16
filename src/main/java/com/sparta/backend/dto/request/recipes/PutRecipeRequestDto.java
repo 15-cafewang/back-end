45c36f2443f799fac1dc888aea39e4177dc555e9
heejun <heejjuunn@gmail.com>
2021-11-16 21:22:16 +0900

    fix: 레시피 수정 dto null처리

--- src/main/java/com/sparta/backend/dto/request/recipes/PutRecipeRequestDto.java
+++ src/main/java/com/sparta/backend/dto/request/recipes/PutRecipeRequestDto.java
@@ -1,9 +1,12 @@
 package com.sparta.backend.dto.request.recipes;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Getter
